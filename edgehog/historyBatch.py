66989f739a6f33af194816919ce0c6a693e5040e
Luca Foppiano <luca@foppiano.org>
2018-07-13 14:39:33 +0200

    adding html output

--- edgehog/historyBatch.py
+++ edgehog/historyBatch.py
@@ -34,19 +34,45 @@ with open(output + ".raw", 'a') as rawOutput:
 header = ['rawName', 'type', 'offsetStart', 'offsetEnd', 'nerd_selection_score', 'wikipediaExternalRef', 'wikidataId']
 
 toCSV = []
-for entity in result[0]['entities']:
+entities_ = result[0]['entities']
+for entity in entities_:
     outEntity = {}
+    # Write 'header' content
     for headerElement in header:
         if headerElement in entity:
             outEntity[headerElement] = entity[headerElement]
         else:
             outEntity[headerElement] = ''
+    # Write additional content
 
     toCSV.append(outEntity)
 
+## Writing CSV
 import csv
 
 with open(output + ".csv", 'w') as csvOutput:
     writer = csv.DictWriter(csvOutput, toCSV[0].keys())
     writer.writeheader()
     writer.writerows(toCSV)
+
+## Output HTML
+indexText = len(string)
+html = ''
+
+for i in reversed(range(0, len(entities_))):
+    entity = entities_[i]
+    start = entity['offsetStart']
+    end = entity['offsetEnd']
+
+    while indexText > end:
+        indexText = indexText - 1
+        html = string[indexText] + html
+
+    html = '<mark>' + string[start:end] + '</mark>' + html
+
+    indexText = start
+
+with open(output + ".html", 'w') as htmlOutput:
+    htmlOutput.write('<html><header><meta charset="utf-8"/></header><body>\n'
+                     + html
+                     + '\n</body></html>')
