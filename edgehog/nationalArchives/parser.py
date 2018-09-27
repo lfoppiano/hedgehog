594c50d85654b743d3e36ec954d24332b1b598b3
lfoppiano <luca@foppiano.org>
2018-09-27 12:04:16 +0200

    adding output

--- edgehog/nationalArchives/parser.py
+++ edgehog/nationalArchives/parser.py
@@ -1,11 +1,52 @@
 import sys
 from bs4 import BeautifulSoup
 
-if len(sys.argv) != 3:
-    sys.exit("Not enough args. Usage: historyBatch.py input output")
+# if len(sys.argv) != 3:
+#     sys.exit("Not enough args. Usage: historyBatch.py input output")
 
+# input directory
 input = sys.argv[1]
-output = sys.argv[2]
+
+# output directory
+# output = sys.argv[2]
+
+listEntities = [("Pope", 'PERSON'), ("Darth Wader", 'PERSON'), ('Clamart', 'LOCATION')]
+
+soup = BeautifulSoup(open(input), 'xml')
+archdesc = soup.ead.archdesc
+
+controlAccess = soup.new_tag("controlaccess")
+archdesc.insert(len(archdesc.contents), controlAccess)
+
+## corpname, famname, function, genreform, geogname,name, occupation, persname, subject,
 
 
+mapping = {
+    # 'famname': ['PERSON'],
+    # 'function': []
+    # 'genreform',
+    'geogname': ['LOCATION'],
+    # 'name': ['PERSON'],
+    # 'occupation':
+    'persname': ['PERSON'],
+    # 'subject': ['UNKNOWN', None, 'ARTIFACT', 'ANIMAL', 'PLANT', 'WEBSITE'],
+    'corpname': ['BUSINESS', 'INSTITUTION', 'ORGANISATION']
+}
 
+inverseMapping = {}
+for k, v in mapping.items():
+    for x in v:
+        inverseMapping.setdefault(x, []).append(k)
+
+# print(inverseMapping)
+
+for entity in listEntities:
+    tag = inverseMapping.get(entity[1])
+    if tag is None:
+        tag = 'subject'
+    entityTag = soup.new_tag(tag[0])
+    entityTag.string = entity[0]
+    controlAccess.insert_after(entityTag)
+
+print(archdesc)
+# print(soup)
